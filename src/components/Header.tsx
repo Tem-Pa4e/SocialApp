784d75473d692e1d14dfac601ebf412771d4f7a2
Tem-Pa4e <sashachumasov@yahoo.com>
2021-10-13 02:46:59 +0300

    first components

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <header className='header'>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1724px-Apple_logo_black.svg.png"
+                alt=""/>
+        </header>
+    )
+}
+
+
+export default Header
---
+++ src/components/Profile.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+function Profile() {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://www.michigannature.org/images/img_library/crp/1-132-7-slider-bg-2.jpg"
+                    alt=""/>
+            </div>
+            <div>
+                ava
+            </div>
+            <div>
+                My Posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    Post1
+                </div>
+                <div>
+                    Post2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Profile
